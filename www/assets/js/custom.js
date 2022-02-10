e68319120e75de8f4e91f8910fef2a65c64c24b0
hacker <hacker@kali>
2022-02-10 12:36:43 +0000

    buy section

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -84,11 +84,11 @@ document.querySelector("#tab-head-cuplumps").addEventListener('click', (e) => {
     setAsActive(current_buy_tab_head);
 })
 
-document.querySelector("#tab-head-raw-latex").addEventListener('click', () => {
+document.querySelector("#tab-head-buy-raw-latex").addEventListener('click', () => {
     hideElement(current_buy_section);
     setAsInactive(current_buy_tab_head);
-    current_buy_section = "#tab-content-raw-latex";
-    current_buy_tab_head = "#tab-head-raw-latex";
+    current_buy_section = "#tab-content-buy-raw-latex";
+    current_buy_tab_head = "#tab-head-buy-raw-latex";
     showElement(current_buy_section);
     setAsActive(current_buy_tab_head);
 })

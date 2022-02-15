4b0df25f1178714e574b1a7e8168f4f5fabb0102
hacker <hacker@kali>
2022-02-15 23:03:44 +0000

    login page

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,21 @@
 (function($) {
 
-	"use strict";
+    "use strict";
 
 
 })(jQuery);
+
+
+
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "Formget" && password == "formget#123") {
+        // alert("Login successfully");
+        window.location = "index.html"; // Redirecting to other page.
+        return false;
+    } else {
+
+        alert("Wrong Password, Please check login information and try again.");
+    }
+}
